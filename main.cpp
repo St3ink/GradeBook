7b2e6d33ca64e3247eb71c2c6185ea4ba1a53dcc
Kason Steiner <kasonsteiner27@gmail.com>
2025-02-11 00:51:57 +0000

    Assistant checkpoint: Fix name reading in arrayAssign function
    Assistant generated file changes:
    - main.cpp: Fix double reading of names in arrayAssign
    
    ---
    
    User prompt:
    
    Running the code in my project results in the following:
    
    ```
    Briana Devon Javier Johnathan Vanessa  87 97 82 92 94
    92 98 91 100 85
    85 82 72 84 85
    78 60 63 79 81
    74 91 77 74 80
    32663 1193338125 32663 0 0
    
    ```
    
    If applicable, propose a fix immediately.

--- main.cpp
+++ main.cpp
@@ -34,12 +34,11 @@ int arrayAssign(string names[6], int grades[6][5]){
   ifstream inputFile("studentGrades.txt");
   
     for (int i = 0; i < 6; i++) {
-      (getline(inputFile, names[i]));
-      inputFile >> names[i];
-        for(int j = 0; j < 5; j++){
-          inputFile >> grades[i][j];
-        }
-     
+      getline(inputFile, names[i]);
+      for(int j = 0; j < 5; j++) {
+        inputFile >> grades[i][j];
+      }
+      inputFile.ignore(); // Skip newline after grades
     }
   return 0;
 }
